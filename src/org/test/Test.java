625508413af15ca505ab18d37e30a365ed546d1b
rubashree31 <rubashreeraja31@gmail.com>
2023-02-04 20:23:10 +0530

    suvas

--- src/org/test/Test.java
+++ src/org/test/Test.java
@@ -7,11 +7,20 @@ public class Test {
 		System.out.println("test1");
 
 	}
+	
+	private void test3() {
+		
+		int a=5,b=5;
+		int add = a + b;
+		System.out.println(add);
+
+	}
 
 	public static void main(String[] args) {
 		
 		
 		Test test = new Test();
 		test.test1();
+		test.test3();
 	}
 }
